60f4192e3a58352914a68c40cecbb219be8fb26e
George Panagopoulos <georgepanagopoulos5@yahoo.gr>
2024-08-27 16:27:41 +0200

    Update prepare_data.py

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -215,11 +215,6 @@ class MovieLens25(InMemoryDataset):
 
     def download(self):
         local_filename = self.url.split('/')[-1]
-        #with requests.get(url, stream=True) as r:
-        #    r.raise_for_status()
-        #    with open(local_filename, 'wb') as f:
-        #        for chunk in r.iter_content(chunk_size=8192): 
-        #            f.write(chunk)
         
         download_url(f'{self.url}', self.raw_dir)
         # Unzip the file
